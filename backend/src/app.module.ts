d4d8cd381fb595fb25c691dea4cd55ee87476a25
AlvMauri <maurirada987@gmail.com>
2026-06-07 17:00:34 -0400

    fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { HealthController } from './health.controller';
 import { AuthModule } from './auth/auth.module';
-import { LogAccesoModule } from './log-acceso/log-acceso.module';
 import { ClientesModule } from './clientes/clientes.module';
-  import { PlanesMembresiaModule } from
-  './planes-membresia/planes-membresia.module';
-  import { MembresiasModule } from './membresias/membresias.module';
-import { InventarioModule } from './inventario/inventario.module';
-import { VentasModule } from './ventas/ventas.module';
-import { EquipamientoModule } from './equipamiento/equipamiento.module';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { EquipamientoModule } from './equipamiento/equipamiento.module';
+import { HealthController } from './health.controller';
+import { InventarioModule } from './inventario/inventario.module';
+import { LogAccesoModule } from './log-acceso/log-acceso.module';
+import { MembresiasModule } from './membresias/membresias.module';
+import { PlanesMembresiaModule } from './planes-membresia/planes-membresia.module';
 import { ReportesModule } from './reportes/reportes.module';
+import { VentasModule } from './ventas/ventas.module';
 
 @Module({
   imports: [
@@ -23,8 +22,8 @@ import { ReportesModule } from './reportes/reportes.module';
         type: 'postgres',
         host: config.get('DB_HOST'),
         port: config.get<number>('DB_PORT'),
-        username: config.get('DB_USER'),
-        password: config.get('DB_PASS'),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
         database: config.get('DB_NAME'),
         autoLoadEntities: true,
         synchronize: true,
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,17 @@
- import { NestFactory } from '@nestjs/core';
-  import { AppModule } from './app.module';
-  import { ValidationPipe } from '@nestjs/common';
+ import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
   async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-    app.enableCors();
+      app.enableCors(
+    {
+      origin: process.env.CORS_ORIGIN ??'http://localhost:5173',
+      methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    })
     await app.listen(3000);
+
   }
   bootstrap();
